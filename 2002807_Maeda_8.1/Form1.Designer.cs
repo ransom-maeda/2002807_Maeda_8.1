6a0fcb211ccb8abe6efebc1ee1e227a864d5c2c6
Ransom Maeda <random.maeda@gmail.com>
2019-05-20 19:40:51 -0700

    commit

--- 2002807_Maeda_8.1/Form1.Designer.cs
+++ 2002807_Maeda_8.1/Form1.Designer.cs
@@ -28,13 +28,182 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.enterDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelSize = new System.Windows.Forms.Label();
+            this.sizeInput = new System.Windows.Forms.TextBox();
+            this.labelCode = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelIncome = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.reportsToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(800, 24);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.enterDataToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // reportsToolStripMenuItem
+            // 
+            this.reportsToolStripMenuItem.Name = "reportsToolStripMenuItem";
+            this.reportsToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.reportsToolStripMenuItem.Text = "&Reports";
+            this.reportsToolStripMenuItem.Click += new System.EventHandler(this.reportsToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // enterDataToolStripMenuItem
+            // 
+            this.enterDataToolStripMenuItem.Name = "enterDataToolStripMenuItem";
+            this.enterDataToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.enterDataToolStripMenuItem.Text = "&Enter Data";
+            this.enterDataToolStripMenuItem.Click += new System.EventHandler(this.enterDataToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(22, 20);
+            this.toolStripMenuItem1.Text = " ";
+            // 
+            // labelSize
+            // 
+            this.labelSize.AutoSize = true;
+            this.labelSize.Location = new System.Drawing.Point(13, 95);
+            this.labelSize.Name = "labelSize";
+            this.labelSize.Size = new System.Drawing.Size(59, 13);
+            this.labelSize.TabIndex = 1;
+            this.labelSize.Text = "Family Size";
+            // 
+            // sizeInput
+            // 
+            this.sizeInput.Location = new System.Drawing.Point(128, 92);
+            this.sizeInput.Name = "sizeInput";
+            this.sizeInput.Size = new System.Drawing.Size(100, 20);
+            this.sizeInput.TabIndex = 2;
+            // 
+            // labelCode
+            // 
+            this.labelCode.AutoSize = true;
+            this.labelCode.Location = new System.Drawing.Point(13, 128);
+            this.labelCode.Name = "labelCode";
+            this.labelCode.Size = new System.Drawing.Size(95, 13);
+            this.labelCode.TabIndex = 3;
+            this.labelCode.Text = "Identification Code";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // labelIncome
+            // 
+            this.labelIncome.AutoSize = true;
+            this.labelIncome.Location = new System.Drawing.Point(13, 161);
+            this.labelIncome.Name = "labelIncome";
+            this.labelIncome.Size = new System.Drawing.Size(74, 13);
+            this.labelIncome.TabIndex = 5;
+            this.labelIncome.Text = "Yearly Income";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(128, 158);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.labelIncome);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelCode);
+            this.Controls.Add(this.sizeInput);
+            this.Controls.Add(this.labelSize);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menu;
+            this.Name = "Form1";
+            this.Text = "8.1";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem enterDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Label labelSize;
+        private System.Windows.Forms.TextBox sizeInput;
+        private System.Windows.Forms.Label labelCode;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelIncome;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- 2002807_Maeda_8.1/Form1.cs
+++ 2002807_Maeda_8.1/Form1.cs
@@ -12,9 +12,90 @@ namespace _2002807_Maeda_8._1
 {
     public partial class Form1 : Form
     {
+        int familySize,
+            income;
+        
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            if(familySize == 1)
+            {
+                income = 10210;
+            }
+            else if(familySize == 2)
+            {
+                income = 13690;
+            }
+            else if (familySize == 3)
+            {
+                income = 17170;
+            }
+            else if (familySize == 4)
+            {
+                income = 20650;
+            }
+            else if (familySize == 5)
+            {
+                income = 24130;
+            }
+            else if (familySize == 6)
+            {
+                income = 27610;
+            }
+            else if (familySize == 7)
+            {
+                income = 31090;
+            }
+            else if (familySize == 8)
+            {
+                income = 34570;
+            }
+            else if(familySize > 8)
+            {
+                income = 34570;
+                income += 3480;
+            }
+        }
+
+        private void enterDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reportsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Font printFont = new Font("Arial", 10);
+            Font headingFont = new Font("Arial", 14, FontStyle.Bold);
+            float verticalPrintPositionFloat = e.MarginBounds.Top;
+            float horizontalPrintPositionFloat = e.MarginBounds.Left;
+            float lineHeightFloat = printFont.GetHeight();
+
+            e.Graphics.DrawString("ID Number ", headingFont, Brushes.Black, horizontalPrintPositionFloat, verticalPrintPositionFloat);
+            verticalPrintPositionFloat += 2 * lineHeightFloat;
+
+            for(int indexInteger = 0; indexInteger < familySize; indexInteger++)
+            {
+                e.Graphics.DrawString(familySize.ToString(), printFont, Brushes.Black, horizontalPrintPositionFloat, verticalPrintPositionFloat);
+                verticalPrintPositionFloat += lineHeightFloat;
+            }
+        }
+
+        
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Program name - 8.1 Survey \nProgrammed by - Ransom Maeda");
         }
     }
 }
